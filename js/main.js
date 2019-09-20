edc970590c5c11b4fae8aad8fb20b907a7c8c112
anyakul <anyakulikova111@gmail.com>
2019-09-20 23:59:49 +0300

    Делаю показ фотографий владельцев объявлений на карте

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
-var user = [
-  'user1',
-  'user2',
-  'user3',
-  'user4',
-  'user5',
-  'user6',
-  'user7',
-  'user8'
-]
+'use strict';
 
 var avatar = [
   'img/avatars/user01.png',
@@ -18,19 +9,19 @@ var avatar = [
   'img/avatars/user06.png',
   'img/avatars/user07.png',
   'img/avatars/user08.png'
-]
+];
 
-X_MIN = 0,
-X_MAX = 1200,
-Y_MIN = 130,
-Y_MAX = 630
+var X_MIN = 0;
+var X_MAX = 1200;
+var Y_MIN = 130;
+var Y_MAX = 630;
 
 // Функция получения рандомных чисел в определенном диапозоне
 var getRandomNumber = function (valueMin, valueMax) {
   return Math.floor(Math.random() * (valueMax - valueMin) + valueMin);
 };
 
-//Делаем карту активной
+// Делаем карту активной
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
@@ -38,29 +29,28 @@ var mapPins = document.querySelector('.map__pins');
 
 var similarPinsTemplate = document.querySelector('#pin')
     .content
-    .querySelector('.map__pin'); 
-  
+    .querySelector('.map__pin');
+
 var fragment = document.createDocumentFragment();
 
-//Показываем фотографии на карте
-for (i = 0; i < user.length; i++) {
-  
+// Показываем фотографии на карте
+for (var i = 0; i < avatar.length; i++) {
+
   var pinsElement = similarPinsTemplate.cloneNode(true);
-  
+
   var pinsElementImg = similarPinsTemplate.querySelector('img');
-  
+
   var numberUser = getRandomNumber(1, avatar.length);
-  
+
   var leftValue = getRandomNumber(X_MIN, X_MAX);
-  
+
   var topValue = getRandomNumber(Y_MIN, Y_MAX);
-  
-  
-  pinsElementImg.src = "img/avatars/user0" + numberUser + ".png";
-  pinsElement.style.left = leftValue + "px";
-  pinsElement.style.top = topValue + "px";
-  
-  fragment.appendChild(pinsElement);   
+
+  pinsElementImg.src = 'img/avatars/user0' + numberUser + '.png';
+  pinsElement.style.left = leftValue + 'px';
+  pinsElement.style.top = topValue + 'px';
+
+  fragment.appendChild(pinsElement);
 }
- 
+
 mapPins.appendChild(fragment);
